d5a3fc432f1f8cc05e0fce43e8daaf8b2dd314d4
jim12312321 <jim12312321@gmail.com>
2022-03-13 14:17:06 +0800

    Refactor functions and add suppression
    @client.c: Let max of fib sequence up to 500.
    @fibdrv.c: Use string to implement big number addition.
    	| @_reverse(x): Reverse string x.
    	| @_max(a,b): return max of a and b.
    	| @string_add(char *a,char *b, char *out): out = a+b.
    	| @node_init(struct fib_node *_node, char *str):
    		- kmalloc a new fib_node and init data with str.
    	| @fib_seq_str(long long k,char *out): Calculate fib sequence.
    @scripts/pre-commit.hook:
    	- Add suppression of variableScope and shiftTooManyBitsSigned.
    @scripts/verify.py: Modify the check-fib range for checking 1 to 500.

--- client.c
+++ client.c
@@ -3,17 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 #define FIB_DEV "/dev/fibonacci"
 
 int main()
 {
+    // cppcheck-suppress variableScope
     long long sz;
+    struct timespec ts1, ts2;
 
-    char buf[1];
+    char buf[256];
     char write_buf[] = "testing writing";
-    int offset = 100; /* TODO: try test something bigger than the limit */
+    int offset = 500; /* TODO: try test something bigger than the limit */
 
     int fd = open(FIB_DEV, O_RDWR);
     if (fd < 0) {
@@ -23,16 +26,19 @@ int main()
 
     for (int i = 0; i <= offset; i++) {
         lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
+        clock_gettime(CLOCK_REALTIME, &ts1);
+        sz = read(fd, buf, sizeof(buf));
+        buf[sz] = 0;
+        clock_gettime(CLOCK_REALTIME, &ts2);
         printf("Reading from " FIB_DEV
                " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
+               "%s. ",
+               i, buf);
         sz = write(fd, write_buf, strlen(write_buf));
-        printf("cost: %lld ns\n", sz);
+        printf("cost time in kernel: %lld ns,", sz);
+        printf("cost time in userspace: %ld ns.\n", ts2.tv_nsec - ts1.tv_nsec);
     }
 
     close(fd);
-    free(sz);
     return 0;
 }
--- fibdrv.c
+++ fibdrv.c
@@ -7,18 +7,34 @@
 #include <linux/ktime.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
+#include <linux/slab.h>
+#include <linux/string.h>
 
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_AUTHOR("National Cheng Kung University, Taiwan");
 MODULE_DESCRIPTION("Fibonacci engine driver");
-MODULE_VERSION("0.1");
+MODULE_VERSION("0.11");
 
 #define DEV_FIBONACCI_NAME "fibonacci"
 
 /* MAX_LENGTH is set to 92 because
  * ssize_t can't fit the number > 92
  */
-#define MAX_LENGTH 92
+#define MAX_LENGTH 500
+
+/* swap string x */
+#define _reverse(x)                                \
+    do {                                           \
+        for (int i = 0; i < strlen(x) >> 1; i++) { \
+            if (x[i] == x[strlen(x) - i - 1])      \
+                continue;                          \
+            x[i] ^= x[strlen(x) - i - 1];          \
+            x[strlen(x) - i - 1] ^= x[i];          \
+            x[i] ^= x[strlen(x) - i - 1];          \
+        }                                          \
+    } while (0)
+
+#define _max(a, b) (a ^ ((a ^ b) & -(a < b)))
 
 static dev_t fib_dev = 0;
 static struct cdev *fib_cdev;
@@ -27,28 +43,83 @@ static DEFINE_MUTEX(fib_mutex);
 
 static ktime_t kt;
 
-static long long fib_sequence(long long k)
+typedef struct fib_node {
+    char data[256];
+} fib_node;
+
+/*
+ * add string a and b mathematically.
+ * add the char in a and b backward,and record carry if necessary.
+ * reverse the answer in the end.
+ */
+static void string_add(char *a, char *b, char *out)
 {
-    /* FIXME: use clz/ctz and fast algorithms to speed up */
-    long long f[k + 2];
-
-    f[0] = 0;
-    f[1] = 1;
-
-    for (int i = 2; i <= k; i++) {
-        f[i] = f[i - 1] + f[i - 2];
+    short tmp = 0, carry = 0;
+    long len_a = strlen(a), len_b = strlen(b);
+    long len_max = _max(len_a, len_b);
+    for (int i = 0; i < len_max; i++) {
+        // cppcheck-suppress shiftTooManyBitsSigned
+        tmp = ((len_a - i - 1) >> 63) ? 0 : (a[len_a - i - 1] - '0');
+        // cppcheck-suppress shiftTooManyBitsSigned
+        tmp += ((len_b - i - 1) >> 63) ? 0 : (b[len_b - i - 1] - '0');
+        if (carry)
+            tmp += carry;
+        if (tmp >= 10) {
+            tmp -= 10;
+            carry = 1;
+        } else {
+            carry = 0;
+        }
+        out[i] = tmp + '0';
     }
-
-    return f[k];
+    if (carry) {
+        out[len_max] = carry + '0';
+        len_max++;
+    }
+    out[len_max + 1] = '\0';
+    _reverse(out);
 }
 
-static long long fib_time_proxy(long long k)
+/*
+ * Init node with string.
+ */
+static struct fib_node *node_init(struct fib_node *_node, char *str)
+{
+    _node = kmalloc(sizeof(fib_node), GFP_KERNEL);
+    strncpy(_node->data, str, strlen(str) + 1);
+    _node->data[strlen(str) + 1] = '\0';
+    return _node;
+}
+
+static int fib_seq_str(long long k, char *out)
+{
+    /* FIXME: use clz/ctz and fast algorithms to speed up */
+    struct fib_node *f[k + 2];
+    int n;
+    f[0] = node_init(f[0], "0");
+    f[1] = node_init(f[1], "1");
+
+    for (int i = 2; i <= k; i++) {
+        f[i] = node_init(f[i], "0");
+        string_add(f[i - 2]->data, f[i - 1]->data, f[i]->data);
+    }
+
+    n = strlen(f[k]->data);
+    if (copy_to_user(out, f[k]->data, n))
+        return -EFAULT;
+
+    for (int i = 0; i <= k; i++)
+        kfree(f[i]);
+
+    return n;
+}
+
+static int fib_time_proxy_t(long long k, char *out)
 {
     kt = ktime_get();
-    long long result = fib_sequence(k);
+    int n = fib_seq_str(k, out);
     kt = ktime_sub(ktime_get(), kt);
-
-    return result;
+    return n;
 }
 
 static int fib_open(struct inode *inode, struct file *file)
@@ -72,10 +143,10 @@ static ssize_t fib_read(struct file *file,
                         size_t size,
                         loff_t *offset)
 {
-    return (ssize_t) fib_time_proxy(*offset);
+    return (ssize_t) fib_time_proxy_t(*offset, buf);
 }
 
-/* write operation is skipped */
+/* return cost time (ns) */
 static ssize_t fib_write(struct file *file,
                          const char *buf,
                          size_t size,
